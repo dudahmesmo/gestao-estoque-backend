2abfee0a25250ac33a524df9ac61e671f0050b47
kaue-santana <kauesantana2308@gmail.com>
2025-11-20 14:43:28 -0300

    Update Ferramenta.java
    Quantidade de estoque, quantidade mínima e quantidade máxima.

--- src/main/java/br/unisul/a3sdm/gestao_estoque_backend/model/Ferramenta.java
+++ src/main/java/br/unisul/a3sdm/gestao_estoque_backend/model/Ferramenta.java
@@ -16,6 +16,12 @@ public class Ferramenta {
     private String marca;
 
     private double preco;
+    
+    private int quantidade_estoque;
+    
+    private int quantidade_minima;
+    
+    private int quantidade_maxima;
 
     private Boolean disponivel;
     
@@ -39,6 +45,15 @@ public class Ferramenta {
 
     public double getPreco() { return preco; }
     public void setPreco(double preco) { this.preco = preco; }
+    
+    public int getQuantidade_estoque() { return quantidade_estoque; }
+    public void setQuantidade_estoque(int quantidade_estoque) { this.quantidade_estoque = quantidade_estoque; }
+
+    public int getQuantidade_minima() { return quantidade_minima; }
+    public void setQuantidade_minima(int quantidade_minima) { this.quantidade_minima = quantidade_minima; }
+
+    public int getQuantidade_maxima() { return quantidade_maxima; }
+    public void setQuantidade_maxima(int quantidade_maxima) { this.quantidade_maxima = quantidade_maxima; }
 
     public Boolean getDisponivel() { return disponivel; }
     public void setDisponivel(Boolean disponivel) { this.disponivel = disponivel; }
