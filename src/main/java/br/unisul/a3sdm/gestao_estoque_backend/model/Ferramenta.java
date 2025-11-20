bffcd13b53e58bb894b8e2641f635db02cb8b86d
Miaunisul <millenaunisul@gmail.com>
2025-11-20 19:22:34 -0300

    chore(db): correção do Ferramentas.java

--- src/main/java/br/unisul/a3sdm/gestao_estoque_backend/model/Ferramenta.java
+++ src/main/java/br/unisul/a3sdm/gestao_estoque_backend/model/Ferramenta.java
@@ -1,62 +1,29 @@
 package br.unisul.a3sdm.gestao_estoque_backend.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "ferramentas")
+@Table(name = "ferramenta")
 public class Ferramenta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_ferramenta")
     private Long id;
 
-    @Column(name = "nome_ferramenta", nullable = false)
     private String nome;
 
-    @Column(name = "marca_ferramenta")
     private String marca;
 
-<<<<<<< HEAD
-    @Column(nullable = false)
-    private double preco;
-
-    @Column(name = "quantidade_estoque", nullable = false)
-    private int quantidadeEstoque;
-
-    @Column(name = "quantidade_minima", nullable = false)
-    private int quantidadeMinima;
-
-    @Column(name = "quantidade_maxima", nullable = false)
-    private int quantidadeMaxima;
-
-    private Boolean disponivel;
-
-    @Column(name = "data_cadastro")
-    private LocalDateTime dataCadastro;
-
-    @PrePersist
-    protected void onCreate() {
-        dataCadastro = LocalDateTime.now();
-    }
-
-    // GETTERS E SETTERS ----------------------
-
-=======
     private Double preco;
 
-    private Integer quantidade;
-
+    @Column(name = "quantidade_estoque")
     private Integer quantidadeEstoque;
+
+    @Column(name = "quantidade_minima_estoque")
     private Integer quantidadeMinimaEstoque;
+
+    @Column(name = "quantidade_maxima_estoque")
     private Integer quantidadeMaximaEstoque;
-
-    @Column(name = "emprestada")
-    private Boolean emprestada = false;
-
-    @Column(name = "data_cadastro")
-    private LocalDateTime dataCadastro = LocalDateTime.now();
 
     private Boolean disponivel = true;
 
@@ -67,8 +34,6 @@ public class Ferramenta {
     public Ferramenta() {}
 
     // GETTERS E SETTERS
-
->>>>>>> develop
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -78,24 +43,8 @@ public class Ferramenta {
     public String getMarca() { return marca; }
     public void setMarca(String marca) { this.marca = marca; }
 
-<<<<<<< HEAD
-    public double getPreco() { return preco; }
-    public void setPreco(double preco) { this.preco = preco; }
-
-    public int getQuantidadeEstoque() { return quantidadeEstoque; }
-    public void setQuantidadeEstoque(int quantidadeEstoque) { this.quantidadeEstoque = quantidadeEstoque; }
-
-    public int getQuantidadeMinima() { return quantidadeMinima; }
-    public void setQuantidadeMinima(int quantidadeMinima) { this.quantidadeMinima = quantidadeMinima; }
-
-    public int getQuantidadeMaxima() { return quantidadeMaxima; }
-    public void setQuantidadeMaxima(int quantidadeMaxima) { this.quantidadeMaxima = quantidadeMaxima; }
-=======
     public Double getPreco() { return preco; }
     public void setPreco(Double preco) { this.preco = preco; }
-
-    public Integer getQuantidade() { return quantidade; }
-    public void setQuantidade(Integer quantidade) { this.quantidade = quantidade; }
 
     public Integer getQuantidadeEstoque() { return quantidadeEstoque; }
     public void setQuantidadeEstoque(Integer quantidadeEstoque) { this.quantidadeEstoque = quantidadeEstoque; }
@@ -106,21 +55,9 @@ public class Ferramenta {
     public Integer getQuantidadeMaximaEstoque() { return quantidadeMaximaEstoque; }
     public void setQuantidadeMaximaEstoque(Integer quantidadeMaximaEstoque) { this.quantidadeMaximaEstoque = quantidadeMaximaEstoque; }
 
-    public Boolean getEmprestada() { return emprestada; }
-    public void setEmprestada(Boolean emprestada) { this.emprestada = emprestada; }
-
-    public LocalDateTime getDataCadastro() { return dataCadastro; }
-    public void setDataCadastro(LocalDateTime dataCadastro) { this.dataCadastro = dataCadastro; }
->>>>>>> develop
-
     public Boolean getDisponivel() { return disponivel; }
     public void setDisponivel(Boolean disponivel) { this.disponivel = disponivel; }
 
-<<<<<<< HEAD
-    public LocalDateTime getDataCadastro() { return dataCadastro; }
-    public void setDataCadastro(LocalDateTime dataCadastro) { this.dataCadastro = dataCadastro; }
-=======
     public Categoria getCategoria() { return categoria; }
     public void setCategoria(Categoria categoria) { this.categoria = categoria; }
->>>>>>> develop
 }
